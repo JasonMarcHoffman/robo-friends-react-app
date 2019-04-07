e0301bf2d496689616986d26c3828534e01b37e3
JasonMarcHoffman <jasonmarchoffman@gmail.com>
2019-04-07 13:56:39 +0200

    uploading files to github

---
+++ build/precache-manifest.0418d473c39914af2a265422e717606d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8597528e6c42de7db70c",
+    "url": "/static/js/main.fce47433.chunk.js"
+  },
+  {
+    "revision": "efeafe96101d943441f0",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "8597528e6c42de7db70c",
+    "url": "/static/css/main.70b385c7.chunk.css"
+  },
+  {
+    "revision": "efeafe96101d943441f0",
+    "url": "/static/css/2.ac20f839.chunk.css"
+  },
+  {
+    "revision": "dc9f7b71f1c7535d42fc9909265141df",
+    "url": "/index.html"
+  }
+];
